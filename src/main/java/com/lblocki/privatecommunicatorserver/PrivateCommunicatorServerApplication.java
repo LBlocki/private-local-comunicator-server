e034acb5d23024aa299fbc619161fc7220d559ac
Łukasz Błocki <blocki.lukasz@gmail.com>
2021-11-07 18:58:52 +0100

    Git init with base project structure

---
+++ src/main/java/com/lblocki/privatecommunicatorserver/PrivateCommunicatorServerApplication.java
@@ -0,0 +1,13 @@
+package com.lblocki.privatecommunicatorserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrivateCommunicatorServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrivateCommunicatorServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lblocki/privatecommunicatorserver/PrivateCommunicatorServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lblocki.privatecommunicatorserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrivateCommunicatorServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
